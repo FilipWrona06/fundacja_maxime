d5f038dcc24e7702681208e07c12a661456ef251
FilipWrona06 <f.w9@interia.pl>
2025-10-12 21:12:41 +0200

    dodanie next analitics i speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Montserrat } from 'next/font/google';
 import localFont from 'next/font/local';
 import "./globals.css";
@@ -48,6 +50,8 @@ export default function RootLayout({
             {children}
           </main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
