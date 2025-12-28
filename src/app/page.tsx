ab778d489e4bc00319e01ab1b8925f79dddcfcfb
FilipWrona06 <f.w9@interia.pl>
2025-12-28 17:50:04 +0100

    skrócenie home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { About } from "@/components/home/About";
-import { CallToAction } from "@/components/home/CallToAction";
 import { Events } from "@/components/home/Events";
 import { Hero } from "@/components/home/Hero";
-import { Newsletter } from "@/components/home/Newsletter";
 import { Partners } from "@/components/home/Partners";
-import { SocialProof } from "@/components/home/SocialProof";
 import { Support } from "@/components/home/Support";
 import { Timeline } from "@/components/home/Timeline";
 
@@ -18,9 +15,6 @@ export default function Home() {
       <Timeline />
       <Events />
       <Support />
-      <SocialProof />
-      <Newsletter />
-      <CallToAction />
     </main>
   );
 }
